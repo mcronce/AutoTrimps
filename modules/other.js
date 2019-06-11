b8b0da95fb44b84d22d414e6abba61175dd3e213
Mike Cronce <mike@quadra-tec.net>
2019-06-11 09:15:42 -0400

    modules/other.js:  avoidempower():  Whitespace

--- modules/other.js
+++ modules/other.js
@@ -563,11 +563,11 @@ if (game.portal.Anticipation.level > 0) {
 
 function avoidempower() {
 	if(armydeath()) {
-            if (typeof game.global.dailyChallenge.bogged === 'undefined' && typeof game.global.dailyChallenge.plague === 'undefined'){
-                mapsClicked(true);
-                return;
-            }
-        }
+		if (typeof game.global.dailyChallenge.bogged === 'undefined' && typeof game.global.dailyChallenge.plague === 'undefined'){
+			mapsClicked(true);
+			return;
+		}
+	}
 }
 
 var spirebreeding = false;
