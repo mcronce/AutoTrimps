a2c3cb0fe5d0d48e811f3032772b6d441d7b27ff
Mike Cronce <mike@quadra-tec.net>
2019-10-16 14:16:33 -0400

    modules/upgrades.js:  Whitespace, readability, and some minor optimization

--- modules/upgrades.js
+++ modules/upgrades.js
@@ -3,35 +3,49 @@
 var upgradeList = ['Miners', 'Scientists', 'Coordination', 'Speedminer', 'Speedlumber', 'Speedfarming', 'Speedscience', 'Speedexplorer', 'Megaminer', 'Megalumber', 'Megafarming', 'Megascience', 'Efficiency', 'TrainTacular', 'Trainers', 'Explorers', 'Blockmaster', 'Battle', 'Bloodlust', 'Bounty', 'Egg', 'Anger', 'Formations', 'Dominance', 'Barrier', 'UberHut', 'UberHouse', 'UberMansion', 'UberHotel', 'UberResort', 'Trapstorm', 'Gigastation', 'Shieldblock', 'Potency', 'Magmamancers'];
 
 function buyUpgrades() {
-
     for (var upgrade in upgradeList) {
         upgrade = upgradeList[upgrade];
         var gameUpgrade = game.upgrades[upgrade];
         var available = (gameUpgrade.allowed > gameUpgrade.done && canAffordTwoLevel(gameUpgrade));
-	var fuckbuildinggiga = (game.talents.autoStructure.purchased && game.talents.deciBuild.purchased && getPageSetting('hidebuildings')==true && getPageSetting('BuyBuildingsNew')==0);
-		
-	//Coord & Amals
-	if (upgrade == 'Coordination' && (getPageSetting('BuyUpgradesNew') == 2 || !canAffordCoordinationTrimps())) continue;
-	if (upgrade == 'Coordination' && getPageSetting('amalcoord')==true && getPageSetting('amalcoordhd') > 0 && calcHDratio() < getPageSetting('amalcoordhd') && ((getPageSetting('amalcoordt') < 0 && (game.global.world < getPageSetting('amalcoordz') || getPageSetting('amalcoordz') < 0)) || (getPageSetting('amalcoordt') > 0 && getPageSetting('amalcoordt') > game.jobs.Amalgamator.owned && (game.resources.trimps.realMax() / game.resources.trimps.getCurrentSend()) > 2000))) continue;
-	
-	//WS
-	if (
-	    upgrade == 'Coordination' && getEmpowerment() == "Wind" && 
-	    (
-		(getPageSetting('AutoStance') == 3 && game.global.challengeActive != "Daily" && getPageSetting('WindStackingMin') > 0 && game.global.world >= getPageSetting('WindStackingMin') && calcHDratio() < 5) || 
-		(getPageSetting('use3daily') == true && game.global.challengeActive == "Daily" && getPageSetting('dWindStackingMin') > 0 && game.global.world >= getPageSetting('dWindStackingMin') && calcHDratio() < 5)
-	    )
-	) continue;
-	
-	if (
-	    upgrade == 'Coordination' && 
-	    (
-		(getPageSetting('AutoStance') == 3 && game.global.challengeActive != "Daily" && getPageSetting('wsmax') > 0 && getPageSetting('wsmaxhd') > 0 && game.global.world >= getPageSetting('wsmax') && calcHDratio() < getPageSetting('wsmaxhd')) || 
-		(getPageSetting('use3daily') == true && game.global.challengeActive == "Daily" && getPageSetting('dwsmax') > 0 && getPageSetting('dwsmaxhd') > 0 && game.global.world >= getPageSetting('dwsmax') && calcHDratio() < getPageSetting('dwsmaxhd'))
-	    )
-	) continue;
+        var fuckbuildinggiga = (game.talents.autoStructure.purchased && game.talents.deciBuild.purchased && getPageSetting('hidebuildings')==true && getPageSetting('BuyBuildingsNew')==0);
 
-	//Other
+        //Coord
+        if(upgrade == 'Coordination') {
+            if(getPageSetting('BuyUpgradesNew') == 2 || !canAffordCoordinationTrimps()) {
+                continue;
+            }
+            // Amalgamators
+            // TODO:  Make this readable
+            if(getPageSetting('amalcoord') == true && getPageSetting('amalcoordhd') > 0 && calcHDratio() < getPageSetting('amalcoordhd') && ((getPageSetting('amalcoordt') < 0 && (game.global.world < getPageSetting('amalcoordz') || getPageSetting('amalcoordz') < 0)) || (getPageSetting('amalcoordt') > 0 && getPageSetting('amalcoordt') > game.jobs.Amalgamator.owned && (game.resources.trimps.realMax() / game.resources.trimps.getCurrentSend()) > 2000))) {
+                continue;
+            }
+
+            //WS
+            var is_daily = game.global.challengeActive == 'Daily';
+            var is_wind_stance;
+            if(is_daily) {
+                is_wind_stance = getPageSetting('use3daily') == true;
+            } else {
+                is_wind_stance = getPageSetting('AutoStance') == 3;
+            }
+            if(getEmpowerment() == "Wind") {
+                if(calcHDratio() < 5) {
+                    if((!is_daily) && is_wind_stance && getPageSetting('WindStackingMin') > 0 && game.global.world >= getPageSetting('WindStackingMin')) {
+                        continue;
+                    } else if(is_daily && is_wind_stance && getPageSetting('dWindStackingMin') > 0 && game.global.world >= getPageSetting('dWindStackingMin')) {
+                        continue;
+                    }
+                }
+            } else {
+                if((!is_daily) && is_wind_stance && getPageSetting('wsmax') > 0 && getPageSetting('wsmaxhd') > 0 && game.global.world >= getPageSetting('wsmax') && calcHDratio() < getPageSetting('wsmaxhd')) {
+                    continue;
+                } else if(is_daily && is_wind_stance && getPageSetting('dwsmax') > 0 && getPageSetting('dwsmaxhd') > 0 && game.global.world >= getPageSetting('dwsmax') && calcHDratio() < getPageSetting('dwsmaxhd')) {
+                    continue;
+                }
+            }
+        }
+
+        //Other
         if (upgrade == 'Shieldblock' && !getPageSetting('BuyShieldblock')) continue;
         if (upgrade == 'Gigastation' && !fuckbuildinggiga && (game.global.lastWarp ? game.buildings.Warpstation.owned < (Math.floor(game.upgrades.Gigastation.done * getPageSetting('DeltaGigastation')) + getPageSetting('FirstGigastation')) : game.buildings.Warpstation.owned < getPageSetting('FirstGigastation'))) continue;
         if (upgrade == 'Bloodlust' && game.global.challengeActive == 'Scientist' && getPageSetting('BetterAutoFight')) continue;
@@ -48,19 +62,18 @@ function buyUpgrades() {
 var RupgradeList = ['Miners', 'Scientists', 'Coordination', 'Speedminer', 'Speedlumber', 'Speedfarming', 'Speedscience', 'Speedexplorer', 'Megaminer', 'Megalumber', 'Megafarming', 'Megascience', 'Efficiency', 'Explorers', 'Battle', 'Bloodlust', 'Bounty', 'Egg', 'Rage', 'Prismatic', 'Prismalicious', 'Formations', 'Dominance', 'UberHut', 'UberHouse', 'UberMansion', 'UberHotel', 'UberResort', 'Trapstorm', 'Potency'];
 
 function RbuyUpgrades() {
-
     for (var upgrade in RupgradeList) {
         upgrade = RupgradeList[upgrade];
         var gameUpgrade = game.upgrades[upgrade];
         var available = (gameUpgrade.allowed > gameUpgrade.done && canAffordTwoLevel(gameUpgrade));
-			
+
         //Coord
-	if (upgrade == 'Coordination' && (getPageSetting('RBuyUpgradesNew') == 2 || !canAffordCoordinationTrimps())) continue;
+        if (upgrade == 'Coordination' && (getPageSetting('RBuyUpgradesNew') == 2 || !canAffordCoordinationTrimps())) continue;
 
         //Other
         if (!available) continue;
         if (game.upgrades.Scientists.done < game.upgrades.Scientists.allowed && upgrade != 'Scientists') continue;
-            buyUpgrade(upgrade, true, true);
-            debug('Upgraded ' + upgrade, "upgrades", "*upload2");
-        }
+        buyUpgrade(upgrade, true, true);
+        debug('Upgraded ' + upgrade, "upgrades", "*upload2");
+    }
 }
